96183106ffee0200f85b9fbf63007ce4e9dad49a
wenhaowu@Loki <wenhaowu1989@hotmail.com>
2014-12-10 21:30:39 -0800

    Binary tree completed. RB tree and Hash table omitted.

--- ITA/3_Data Structrues/TreeBinary.py
+++ ITA/3_Data Structrues/TreeBinary.py
@@ -11,19 +11,61 @@ class TreeBinary:
     def __init__(self):
         self.root = None
     
+    def search(self, key):
+        node_cur = self.root
+        while node_cur is not None:
+            if key < node_cur.key:
+                node_cur = node_cur.left
+            elif key > node_cur.key:
+                node_cur = node_cur.right
+            else:
+                break
+        return node_cur
+         
     @property
     def max(self):
-        pass
+        if self.root is None:
+            return None
+        node_cur = self.root
+        while node_cur.right is not None:
+            node_cur = node_cur.right
+        return node_cur
 
     @property
     def min(self):
-        pass
+        if self.root is None:
+            return None
+        node_cur = self.root
+        while node_cur.left is not None:
+            node_cur = node_cur.left
+        return node_cur
+         
 
-    def successor(self, key):
-        pass
+    def successor(self, node): # should have been implemented as property of node object
+        if node is None:
+            return None
+        if node.right is not None:
+            tree_tmp = TreeBinary()
+            tree_tmp.root = node.right
+            return tree_tmp.min
+        node_cur = node
+        while node_cur.p is not None and node_cur.p.right == node_cur:
+            node_cur = node_cur.p
+        return node_cur.p 
 
-    def predecessor(self, key):
-        pass
+    def predecessor(self, node):
+         
+        node_cur = node
+        if node_cur is None:
+            return node_cur
+        
+        if node_cur.left is not None:
+            tree_tmp = TreeBinary()
+            tree_tmp.root = node_cur.left
+            return tree_tmp.max
+        while node_cur.p is not None and node_cur == node_cur.p.left:
+            node_cur = node_cur.p
+        return node_cur.p
 
     def insert(self, node):
         node_prev = self.root
@@ -41,9 +83,37 @@ class TreeBinary:
             node_prev.right = node
         else:
             node_prev.left = node
+    
+    def transplant(self, u, v):
+        if u.p is None:
+            self.root = v
+        elif u == u.p.left:
+            u.p.left = v
+        else:
+            u.p.right = v
+        if v is not None:
+            v.p = u.p
 
-    def delete(self, key):
-        pass
+    def delete(self, node):
+        if node is None:
+            return
+        
+        if (node.left, node.right) == (None, None):
+            self.transplant(node, None)
+        elif node.left is None:
+            self.transplant(node, node.right)
+        elif node.right is None:
+            self.transplant(node, node.left)
+        else:
+            node_suc = self.successor(node)
+            if node_suc is not node.right: # not direct right child
+                self.transplant(node_suc.right, node)
+                node_suc.right = node.right
+                node_right.p = node_suc
+            self.transplant(node, node_suc)
+            node_suc.left = node.left
+            node.left.p = node_suc
+        
 
     def walk_in_order(self):
         def walk_in_order_from(node):
--- ITA/3_Data Structrues/test.py
+++ ITA/3_Data Structrues/test.py
@@ -7,30 +7,49 @@ import random
 class TestTreeBinary(unittest.TestCase):
     def setUp(self):
         self.tree = tb.TreeBinary()
-
+        
+        self.keys = [12, 5, 18, 2, 9, 15, 19, 13, 17]
+        for key in self.keys:
+            self.tree.insert(tb.Node(key))  
+             
     def test_search(self):
-        pass
+        self.assertEqual(12, self.tree.search(12).key)
+        self.assertEqual(None, self.tree.search(7))
 
-    def test_min(self):
-        pass
+    def test_min_max(self):
+        self.assertEqual(min(self.keys), self.tree.min.key)
+        self.assertEqual(max(self.keys), self.tree.max.key)        
 
-    def test_max(self):
-        pass
 
     def test_predecessor(self):
-        pass
+        node_1 = self.tree.search(13)
+        self.assertEqual(12, self.tree.predecessor(node_1).key)
+        node_2 = self.tree.search(2)
+        self.assertEqual(None, self.tree.predecessor(node_2))
+        node_3 = self.tree.search(12)
+        self.assertEqual(9, self.tree.predecessor(node_3).key)
 
     def test_successor(self):
-        pass
-
+        node_1 = self.tree.search(9)
+        node_2 = self.tree.search(19)
+        node_3 = self.tree.search(12)
+        
+        self.assertEqual(12, self.tree.successor(node_1).key) 
+        self.assertEqual(None, self.tree.successor(node_2))
+        self.assertEqual(13, self.tree.successor(node_3).key)
     def test_insert_walk(self):
-        keys = (12, 5, 18, 2, 9, 15, 19, 13, 17)
-        for key in keys:
-            self.tree.insert(tb.Node(key))   
-        self.assertEqual(sorted(keys), self.tree.walk_in_order())
+        self.assertEqual(sorted(self.keys), self.tree.walk_in_order())
     
     def test_delete(self):
-        pass
+        node_1 = self.tree.search(2)
+        node_2 = self.tree.search(18)
+        self.tree.delete(node_1)
+        self.keys.remove(2)
+        self.assertEqual(sorted(self.keys), self.tree.walk_in_order())
+        self.tree.delete(node_2)
+        self.keys.remove(18)
+        self.assertEqual(sorted(self.keys), self.tree.walk_in_order())
+          
 
 if __name__ == "__main__":
     unittest.main()
